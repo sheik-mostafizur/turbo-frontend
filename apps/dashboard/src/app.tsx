ca6f183719662c9cab15950ffdaab7a21b44115f
Sheik Mostafizur <sheikmostafizur2001@gmail.com>
2024-10-18 15:22:03 +0600

    feat(app): reactjs dashboard added

---
+++ apps/dashboard/src/app.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./app.css";
+
+function App(): JSX.Element {
+  return (
+    <div className="container">
+      <h1 className="title">
+        Dashboard <br />
+        <span>template by Kitchen Sink</span>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
